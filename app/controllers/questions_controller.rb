426857ff7650e68e9b05f0d92c83126685bc6e1e
Jeremy Sell <thestereoscopics@gmail.com>
2013-08-28 13:35:56 -0700

    merging to master

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,16 @@
+class QuestionsController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
+  def new
+    Question.new(title: params[:title], content: params[:content])
+  end
+
+  # def create
+
+  # end
+
+
+end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Question do
+  let(:question) { question = Question.new(title: "Want good food.", content: "I have raisins, celery and PB. What can I make?") }
+  context "Belongs in Question" do
+    it "has a title" do
+      question.title.should eq "Want good food."
+    end 
+  end
 
 end
