b4ea9a40619a31dbaf2f4af7a8d0f2fb74e6cbff
Apprentice <apprentice@devbootcamp.com>
2013-08-28 10:40:13 -0700

    flesh out migrations

--- db/migrate/20130828173507_create_answers.rb
+++ db/migrate/20130828173507_create_answers.rb
@@ -1,7 +1,11 @@
 class CreateAnswers < ActiveRecord::Migration
   def change
     create_table :answers do |t|
-
+      t.string :content
+      t.integer :score
+      t.belongs_to :user 
+      t.belongs_to :question
+      
       t.timestamps
     end
   end
