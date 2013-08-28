7f491710db27ae289df9c63264e080f8c12a4067
Jeremy Sell <thestereoscopics@gmail.com>
2013-08-28 14:31:18 -0700

    got rid of routes in the question controller that are not yet neccessary

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,12 @@
 class QuestionsController < ApplicationController
 
-  def index
-    @questions = Question.all
-  end
+  # def index
+  #   @questions = Question.all
+  # end
 
-  def new
-    Question.new(title: params[:title], content: params[:content])
-  end
+  # def new
+  #   Question.new(title: params[:title], content: params[:content])
+  # end
 
   # def create
 
