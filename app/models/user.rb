e1181bf4a4565fb2a99dac3e6b7a705955b510fa
Apprentice <apprentice@devbootcamp.com>
2013-08-28 14:46:34 -0700

    tweak user-password spec

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,3 +6,20 @@ class User < ActiveRecord::Base
   validates :email, uniqueness: true, presence: true, format: { with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/ }
   validates :password, presence: true
 end
+
+
+  # def show
+  #   user = User.find_by_username(params[:username])
+  #   if user && user.authenticate(params[:password])
+  #     session[:user_id] = user.id
+  #     redirect_to root_url, :notice => 'logged in'
+  #   else
+  #     flash.now.alert = "Invalid username or password"
+  #     render "new"
+  #   end
+  # end
+
+  # def destroy
+  #   session[:user_id] = nil
+  #   redirect_to root_url, notice => 'Logged out'
+  # end
