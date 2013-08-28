cbae6d1a3f9aa05c1e9812a06386255f01dde84d
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-08-28 10:19:26 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnackOverflow::Application.config.secret_token = '9d97a343c2ab7d997a7810ec5786a49a29807237e10fdbc5720c6cdac22a52204cb7077f7ca19548790edba7049977397b7c955b0c6ca2d7a7d7496c3688ac19'
