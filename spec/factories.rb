65522310fb2940aed1b0569f050a2b2a6f73fcd8
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-08-28 12:34:54 -0700

    add failing tests for answer model

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,4 +4,9 @@ FactoryGirl.define do
     sequence(:email) {|n| "seth#{n}@seth.com" }
     password_hash 'password'
   end
+
+  factory :answer do
+    sequence(:content) {|n| "Answer #{n}"}
+    sequence(:score) { rand(10) }
+  end
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Answer do
+  let(:test_answer) { FactoryGirl.create :answer }
 
+  context "validations" do
+    it { should validate_presence_of :content }
+    it { should validate_presence_of :score }
+    it { should belong_to(:user) }
+    it { should belong_to(:question) }
+  end
+
+  context "content" do
+    it { should respond_to :content }
+  end
+
+  context "score" do
+    it { should respond_to :score }
+  end
 end
