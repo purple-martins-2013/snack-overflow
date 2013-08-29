8f623bc85872bbe96809041c5b09b9131485003c
Jeremy Sell <thestereoscopics@gmail.com>
2013-08-28 20:20:08 -0700

    test to read a particular question

--- spec/features/questions_spec.rb
+++ spec/features/questions_spec.rb
@@ -5,7 +5,7 @@ feature 'Question Layouts' do
      question = Question.create(title: "Pizza, Ricotta, Bourbon", content: "Any clue what I can make with these")
   end
 
-  let(:question) { question = FactoryGirl.create :question, title: "Pizza, Ricotta, Bourbon", content: "Any clue what I can make with these" }
+  let(:question) { question = FactoryGirl.create :question, title: "Pizza, Ricotta, Bourbon", content: "Any clue what I can make with these?" }
 
   context "on welcome page" do 
 
@@ -17,6 +17,7 @@ feature 'Question Layouts' do
     it "can click the title and see the full content" do
       visit questions_path
       click_link "#{question.title}"
+      page.should have_content "Any clue what I can make with these?"
     end
   end
   
