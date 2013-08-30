71281c5af1332b65b1c1428e81d652f04e2180e2
Colleen McCloskey <colleen.michelle.mccloskey@gmail.com>
2013-08-30 15:59:08 -0700

    user may answer question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,11 +1,17 @@
 class AnswersController < ApplicationController
 
   def create
-    
+    @question = Question.find(params[:question_id])
+    answer = Answer.create(params[:answer])
+    if answer.persisted?
+      redirect_to question_path(@question)
+    else
+      render "questions/show", status: :unprocessable_entity
+    end
   end
 
   def destroy
-    
+    redirect_to root_path
   end
 
 
--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,13 +1,38 @@
 require 'spec_helper'
 
 describe AnswersController do
-  let(:answer) { FactoryGirl.create :answer, content: "Ants on a log" }
+  let(:answer) { FactoryGirl.create :answer }
+  let(:question) { FactoryGirl.create :question }
 
   describe "#create" do
-    it { should route(:post, '/answers').to(action: :create) }
+    it { should route(:post, '/questions/1/answers').to(action: :create, question_id: 1) }
+
+    context "when I try to create an answer with empty params" do
+      it "should raise an error" do
+        post :create, {content: { }, question_id: question.id }
+        expect(response.status).to eq 422
+      end
+
+      it "renders the questions/show page" do
+        post :create, {content: { }, question_id: question.id }
+        expect(response).to render_template "questions/show"
+      end
+    end
+
+    context "when I try to create an answer with complete params" do
+      it "should redirect back to question page with answer posted" do
+        post :create, { answer: FactoryGirl.attributes_for(:answer), question_id: question.id }
+        expect(response).to redirect_to(question_path(question))
+      end
+
+    end
   end
 
   describe "#destroy" do
     it { should route(:delete, '/answers/1').to(action: :destroy, id: 1) }
+
+    it "should destroy an answer" do
+      expect {delete :destroy, :id => answer.id }.to change{Answer.all.length}.by(1)
+    end
   end
 end
