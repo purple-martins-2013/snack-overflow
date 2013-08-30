e9b8c402bacf4cf18f9cb3e24c12bee537d7a467
Apprentice <apprentice@devbootcamp.com>
2013-08-30 14:20:07 -0700

    add user controller specs

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -5,10 +5,20 @@ describe QuestionsController do
   
   describe "#index" do
     it { should route(:get, '/').to(action: :index) }
+
+    it "should set variable to all questions" do
+      get 'index'
+      assigns(:questions).should eq Question.all
+    end
   end
 
   describe "#show" do
     it { should route(:get, '/questions/1').to(action: :show, id: 1) }
+
+    it "should set variable to specific question" do
+      get "show", { :id => question.id }
+      assigns(:questions).should_not be_nil
+    end
   end
 
   describe "#new" do
@@ -17,9 +27,18 @@ describe QuestionsController do
 
   describe "#create" do
     it { should route(:post, '/questions').to(action: :create) }
+
+    it "should create a question" do 
+      post "create"
+      expect { Question.create(attributes_for :question) }.to change{ Question.all.length }.by(1)
+    end
   end
 
   describe "#destroy" do
     it { should route(:delete, '/questions/1').to(action: :destroy, id: 1) }
+
+    it "should destroy a question" do
+      expect { delete :destroy, :id => question.id }.should change{Question.all.length}.by(1)
+    end
   end  
 end
