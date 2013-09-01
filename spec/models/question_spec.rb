f12c62d253dd9cb0dccda69fe9e8b61236498fe9
Seth Grotelueschen <sgrotelueschen@westmont.edu>
2013-09-01 12:02:24 -0700

    add user and question has many association tests

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -3,10 +3,14 @@ require 'spec_helper'
 describe Question do
   let(:question) { FactoryGirl.create :question, title: "Want good food.", content: "I have raisins, celery and PB. What can I make?" }
   
+  context "associations" do
+    it { should belong_to :user }
+    it { should have_many :answers}
+  end
+
   context "validations" do
     it { should validate_presence_of :title }
     it { should validate_presence_of :content }
-    it { should belong_to :user }
   end
 
   context "Attributes of a Question" do
