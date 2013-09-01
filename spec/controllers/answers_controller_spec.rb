7b163bc3cc50de68fdee3a437047e8f950773fc2
Clay Whitley <clay.aaron.whitley@gmail.com>
2013-09-01 16:31:13 -0700

    implement upvoting of answers, including specs

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -9,22 +9,31 @@ describe AnswersController do
 
     context "when I try to create an answer with empty params" do
       it "should raise an error" do
-        post :create, {content: { }, question_id: question.id }
+        post :create, { content: { }, question_id: question.id }
         expect(response.status).to eq 422
       end
 
       it "renders the questions/show page" do
-        post :create, {content: { }, question_id: question.id }
+        post :create, { content: { }, question_id: question.id }
         expect(response).to render_template "questions/show"
       end
     end
 
     context "when I try to create an answer with complete params" do
       it "should redirect back to question page with answer posted" do
-        post :create, { answer: attributes_for(:answer), question_id: question.id}
+        post :create, { answer: attributes_for(:answer), question_id: question.id }
         expect(response).to redirect_to(question_path(question))
       end
 
+    end
+  end
+
+  describe "#increment" do
+    it { should route(:put, '/questions/1/answers/1/increment').to(action: :increment, question_id: 1, id: 1) }
+
+    it "should update the score of an answer" do
+      put :increment, { question_id: question.id, id: answer.id }
+      Answer.find_by_id(answer.id).score.should eq 1
     end
   end
 
